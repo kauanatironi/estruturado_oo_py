ff35e1271f7cd422172621cf399d7504895687a8
kauanatironi <kauanaleticia81@gmail.com>
2018-03-16 22:32:54 -0300

    Add files via upload

--- t1_302_kauana_oo.py
+++ t1_302_kauana_oo.py
@@ -1,8 +1,8 @@
 class pessoa():
 
-	def __init__(self, nome, peso, altura): #init para instanciar
-		self.nome = nome
-		self.peso = peso #variavel global
+	def __init__(self, nome, peso, altura): #init para inicializar
+		self.nome = nome #self para variavel global/instanciar
+		self.peso = peso 
 		self.altura = altura
 		self.imc = 0
 
@@ -16,9 +16,13 @@ class pessoa():
 	def __str__(self):
 		return "a" , str(self.nome), "tem" , str(self.peso), "kg, e", str(self.altura), "de altura"
 
+        #def __new__():
+
+        
 
 class roupa(pessoa):
 	def __init__(self, cor, tipo):
 		self.cor = cor
 		self.tipo = tipo
-		super(pessoa, self).__init__(nome, peso, altura)
+		super(pessoa, self).__init__(nome, peso, altura) #herança/ passa os parametros da classe mae/ super classe
+
